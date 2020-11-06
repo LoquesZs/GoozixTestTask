f4c0aa5e7ed067ffc2465256c55fcd4145a74aa4
Anton Polubok <ant.polubok@yandex.rugit config --global user.email ant.polubok@yandex.rugit config --global user.email ant.polubok@yandex.ru>
2020-11-06 10:33:05 +0300

    Initial commit

---
+++ app/src/main/java/com/example/goozixtesttask/ui/main/MainViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.goozixtesttask.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+
+    val API_KEY = "CTnxGn5EhQMdBVjMiiCfdF2RqISxV5vB"
+}
